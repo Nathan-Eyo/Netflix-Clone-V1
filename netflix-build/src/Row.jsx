253e082813c060e3f67cf9393f1420c85d2c4c3e
Nathan-Nsa <nathan.codedev@gmail.com>
2022-10-23 00:51:14 +0100

    "Finished Implementing UI"

--- netflix-build/src/Row.jsx
+++ netflix-build/src/Row.jsx
@@ -1,6 +1,7 @@
-import axios from "./axios"
-import React from 'react'
+import axios from "./axios";
+import React from 'react';
 import { useState, useEffect } from 'react';
+import "./Row.css"
 
 function Row({title, fetchUrl, isLargeRow = false}) {
 
@@ -24,13 +25,17 @@ console.log(movies);
     <div className='row'>
         <h2>{title}</h2>
 
-        <div className="row_posters">
+        <div className='row_posters'>
         {movies.map((movie) => (
-          <img
-          className = {`row_poster ${isLargeRow && "row_posterLarge"}`}
-          key={movie.id}
-           src={`${base_url}${isLargeRow ? movie.poster_path : movie.backdrop_path}`} alt="movie name" />
-        ))}
+          ((isLargeRow && movie.poster_path) ||
+          (!isLargeRow && movie.backdrop_path)) && (
+            <img
+            className = {`row_poster ${isLargeRow && "row_posterLarge"}`}
+            key={movie.id}
+             src={`${base_url}${isLargeRow ? movie.poster_path : movie.backdrop_path}`} 
+             alt={movie.name} />
+          )
+           ))}
       </div>
     </div>
   )
